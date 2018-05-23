335b47096e61a0884bcef3728fec4b69e19b735f
Ali Can Kaynarca <alicankaynarca@gmail.com>
2018-05-24 01:38:38 +0300

    types and actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,16 @@ const ac = (type, payload) => ({type, payload})
 
 export const startup 			= ()						=> ac(types.STARTUP)
 
-export const logIn 				= ()						=> ac(types.LOG_IN)
 export const authStateChanged 	= (user)					=> ac(types.AUTH_STATE_CHANGED, { user })
+export const notLoggedIn 		= ()						=> ac(types.NOT_LOGGED_IN)
+export const loggedIn 			= ()						=> ac(types.LOGGED_IN)
+export const logIn 				= ()						=> ac(types.LOG_IN)
+export const logingIn 			= ()						=> ac(types.LOGGING_IN)
+export const logOut 			= ()						=> ac(types.LOG_OUT)
+export const signUp 			= ()						=> ac(types.SIGN_UP)
+export const sendResetEmail 	= ()						=> ac(types.SEND_RESET_EMAIL)
+export const authError 			= ()						=> ac(types.AUTH_ERROR)
+export const closeAuthError 	= ()						=> ac(types.CLOSE_AUTH_ERROR)
 
 export const routeNavigate 	= (routeName, params = {}) 		=> ac(types.ROUTE_NAVIGATE,{routeName, params})
 export const routeReset 	= (routeName, params = {}) 		=> ac(types.ROUTE_RESET, {routeName, params})
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,7 +1,15 @@
 export const STARTUP				= 'STARTUP'
 
+export const AUTH_STATE_CHANGED		= 'AUTH_STATE_CHANGED'
+export const NOT_LOGGED_IN			= 'NOT_LOGGED_IN'
+export const LOGGED_IN			 	= 'LOGGED_IN'
+export const LOGGING_IN			 	= 'LOGGING_IN'
+export const SIGN_UP			 	= 'SIGN_UP'
 export const LOG_IN			 		= 'LOG_IN'
-export const AUTH_STATE_CHANGED 	= 'AUTH_STATE_CHANGED'
+export const LOG_OUT			 	= 'LOG_OUT'
+export const SEND_RESET_EMAIL		= 'SEND_RESET_EMAIL'
+export const AUTH_ERROR			 	= 'AUTH_ERROR'
+export const CLOSE_AUTH_ERROR		= 'CLOSE_AUTH_ERROR'
 
 export const ROUTE_NAVIGATE 		= 'ROUTE_NAVIGATE'
 export const ROUTE_RESET 			= 'ROUTE_RESET'
